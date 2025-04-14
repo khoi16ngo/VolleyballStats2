01af68aa4906da1546267182b1df2e4d22d7b13f
Khoi <khoi16ngo@gmail.com>
2025-04-13 18:58:08 -0600

    create stat models

---
+++ models/stats/dig_stats.py
@@ -0,0 +1,56 @@
+from models.quality import Quality
+from services.user_input_fetchers.quality_fetcher import find_perfect_quality, find_good_quality, find_ok_quality, find_poor_quality, find_error_quality
+
+class DigStats:
+    def __init__(self):
+        self.perfect_digs = 0
+        self.great_digs = 0
+        self.medium_digs = 0
+        self.poor_digs = 0
+        self.dig_errors = 0
+        self.total_digs = 0
+        self.dig_average = 0.0
+
+    def set_perfect_digs(self, perfect_digs: int) -> None:
+        '''
+        Set the number of perfect digs from the player dig stats.
+        '''
+        self.perfect_digs = perfect_digs
+
+    def set_great_digs(self, great_digs: int) -> None:
+        '''
+        Set the number of great digs from the player dig stats.
+        '''
+        self.great_digs = great_digs
+    
+    def set_medium_digs(self, medium_digs: int) -> None:
+        '''
+        Set the number of medium digs from the player dig stats.
+        '''
+        self.medium_digs = medium_digs
+
+    def set_poor_digs(self, poor_digs: int) -> None:
+        '''
+        Set the number of poor digs from the player dig stats.
+        '''
+        self.poor_digs = poor_digs
+
+    def set_dig_errors(self, dig_errors: int) -> None:
+        '''
+        Set the number of dig errors from the player dig stats.
+        '''
+        self.dig_errors = dig_errors
+
+    def set_total_digs(self, total_digs: int) -> None:
+        '''
+        Set the total number of digs from the player dig stats.
+        '''
+        self.total_digs = total_digs
+
+    def set_dig_average(self, dig_average: float) -> None:
+        '''
+        Set the average of the dig stats.
+        '''
+        self.dig_average = dig_average
+
+    
---
+++ models/stats/serve_receive_stats.py
@@ -0,0 +1,73 @@
+from models.quality import Quality
+from services.user_input_fetchers.quality_fetcher import find_perfect_quality, find_good_quality, find_ok_quality, find_poor_quality, find_error_quality
+
+
+class ServeReceiveStats:
+    def __init__(self):
+        self.perfect_serves = 0
+        self.great_serves = 0
+        self.medium_serves = 0
+        self.poor_serves = 0
+        self.serve_errors = 0
+        self.total_serves = 0
+        self.in_system_percentage = 0.0
+        self.out_of_system_percentage = 0.0
+        self.serve_average = 0.0
+
+    def set_perfect_serves(self, perfect_serves: int) -> None:
+        '''
+        Set the number of perfect serves from the player serve receive stats.
+        '''
+        self.perfect_serves = perfect_serves
+
+    def set_great_serves(self, great_serves: int) -> None:
+        '''
+        Set the number of great serves from the player serve receive stats.
+        '''
+        self.great_serves = great_serves
+
+    def set_medium_serves(self, medium_serves: int) -> None:
+        '''
+        Set the number of medium serves from the player serve receive stats.
+        '''
+        self.medium_serves = medium_serves
+
+    def set_poor_serves(self, poor_serves: int) -> None:
+        '''
+        Set the number of poor serves from the player serve receive stats.
+        '''
+        self.poor_serves = poor_serves
+
+    def set_serve_errors(self, serve_errors: int) -> None:
+        '''
+        Set the number of serve errors from the player serve receive stats.
+        '''
+        self.serve_errors = serve_errors
+
+    def set_total_serves(self, total_serves: int) -> None:
+        '''
+        Set the total number of serves from the player serve receive stats.
+        '''
+        self.total_serves = total_serves
+
+    def set_in_system_percentage(self, in_system_percentage: float) -> None:
+        '''
+        Set the in-system percentage of the serve receive stats.
+        '''
+        self.in_system_percentage = in_system_percentage
+
+    def set_out_of_system_percentage(self, out_of_system_percentage: float) -> None:
+        '''
+        Set the out-of-system percentage of the serve receive stats.
+        '''
+        self.out_of_system_percentage = out_of_system_percentage
+
+    def set_serve_average(self, serve_average: float) -> None:
+        '''
+        Set the average of the serve receive stats.
+        '''
+        self.serve_average = serve_average
+        
+        
+
+    
